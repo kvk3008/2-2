6b369029abb9cbaf12a80c66a338b77a6c491c66
kvk3008 <varunkaja.1234@gmail.com>
2015-01-23 01:37:03 +0530

    my modified carrom board]

--- GRAPHICS/ASS/1/sampleCollision.cpp
+++ GRAPHICS/ASS/1/sampleCollision.cpp
@@ -7,87 +7,102 @@ using namespace std;
 #define PI 3.141592653589
 #define DEG2RAD(deg) (deg * PI / 180)
 float ball_rad = 0.2f;
-int count=0;
+float striker_rad = ball_rad + 0.1f; 
+int   count=0;
+int   score=100;
+float flag2 = 0;
+float flag =0;
+float c_flag = 0;
+float flags[19][19];
+float ball_x = 1.0f;
+float ball_y = 0.0f;
+float ball_velx = 0.01f;
+float ball_vely = 0.02f;
+float box_len = 6.9f;
+float tri_x = 0.0f;
+float tri_y = 0.0f;
+float theta = 0.0f; 
+float ball_x1 = 1.0f;
+float ball_y1 = 1.5f;
+float ball_y3 = -2.0f;
+float ball_x3 = 0.0f;
+float poly_y1 = -( box_len / 2) + 0.5 ;
+float px  = 0,py = 0;
+float o=90.0f;
 class Striker
 {
-  public:
-     float x;
-     float y;
-     float vx;
-     float vy;
-     float m;
+ public:
+  float x,y,vx,vy,m;
+  //Used to draw a line in third quadrant and first quadrant
   void drawl(float x, float y)
   {
     glBegin(GL_LINES);
     glVertex2f(-x,y);
     glVertex2f(x,y);
     glEnd();
-  }   
+  }
 };
 class Coins
 {
-public:
-     float x;
-     float y;
-     int flag;
-     float vx;
-     float vy;  
-     float m;
-     float col;
+  public:
+    float x,y,vx,vy,m,col;
+    int flag;
+  // used to calculate distance betwwen two balls
+  float distance1(float a , float b , float c , float d)
+   { 
+     return sqrt((((a-c)*(a-c))+((b-d)*(b-d))));
+   } 
+   float distance(float a, float b, float c,float d)
+   {
+    return ((a-c)*(a-c)) + ((b-d)*(b-d)); 
+   }
 };
 Striker q;
 Coins c[19];
-float distance1(float a , float b , float c , float d)
-{
-return sqrt((((a-c)*(a-c))+((b-d)*(b-d))));
-}
 int isDragging=0;
 void mouseMove(int x, int y)
 {
-    if (isDragging) {
-        
+  if (isDragging) 
+  {
         float a,b;
         a=(float)x;
         b=(float)y;
         a=(0.0126f)*a-5.769f;
         b=(-0.0131f*b)+3.125f;
-        
         if(a>-2.3f && a<2.5f)
-        q.x = a;
-
-    }
+          q.x = a;
+  }
 }
 void mouseButton(int button, int state, int x, int y)
 {
-    if (button == GLUT_LEFT_BUTTON) {
-        if (state == GLUT_DOWN) { // left mouse button pressed
-            float a,b;
-        a=(float)x;
-        b=(float)y;
-        a=(0.0126f)*a-5.769f;
-        b=(-0.0131f*b)+3.125f;
-            if(distance1(a,b,q.x,q.y) < ball_rad)
-            isDragging = 1; // start dragging
-        }
-        else if(state == GLUT_UP)
-        {
-            isDragging = 0;
-      //      my_striker.velocity_set(indicator.theta, vindicator.current_velocity);
-        }
+  if (button == GLUT_LEFT_BUTTON) 
+  {
+    if (state == GLUT_DOWN) 
+    { 
+      // left mouse button pressed
+      float a,b;
+      a=(float)x;
+      b=(float)y;
+      a=(0.0126f)*a-5.769f;
+      b=(-0.0131f*b)+3.125f;
+      if(c[0].distance1(a,b,q.x,q.y) < ball_rad)
+        isDragging = 1; // start dragging
     }
-    if(button == GLUT_RIGHT_BUTTON)
+    else if(state == GLUT_UP)
     {
+      isDragging = 0;
+    }
+  }
+  if(button == GLUT_RIGHT_BUTTON)
+  {
         float a,b;
         a=(float)x;
         b=(float)y;
         a=(0.0126f)*a-5.769f;
         b=(-0.0131f*b)+3.125f;
-        
-      
-    }
+  }
 }
 // Function Declarations
-float flag2=0;
 void drawScene();
 void update(int value);
 void drawBox(float len);
@@ -102,38 +117,7 @@ void handleMouseclick(int button, int state, int x, int y);
 void rectangle(float len);
 void drawcircle(float rad);
 void drawcircle2(float rad); 
-float distance(float x1,float y1,float x2,float y2)
-{
-  float temp;
-  temp=((x1-x2)*(x1-x2))+((y1-y2)*(y1-y2));
-  temp=sqrt(temp);
-  return temp;
-}
-int score=100;
-float flag =0;
-float c_flag = 0;
-float ball_x = 1.0f;
-float ball_y = 0.0f;
-float ball_velx = 0.01f;
-float ball_vely = 0.02f;
-int flags[19][19];
-//float ball_rad = 0.2f;
-float box_len = 6.9f;
-float tri_x = 0.0f;
-float tri_y = 0.0f;
-float theta = 0.0f; 
-float ball_x1 = 1.0f;
-float ball_y1 = 1.5f;
-float ball_velx1 = 0.015f;
-float ball_vely1 = 0.022f;
-float ball_velx3 = 0.015f; 
-float ball_vely3 = 0.025f;
-float ball_y3 = -2.0f;
-float ball_x3 = 0.0f;
-float poly_y1 = -( box_len / 2) + 0.5 ;
-float px  = 0,py = 0;
-int o=90;
-void Sprint( int x, int y)
+void Sprint( float x, float y)
 {
  glColor3f(1.0f, 0.0f, 0.0f);
  int l,i=0;
@@ -141,41 +125,41 @@ void Sprint( int x, int y)
  int temp = score;
  int t=0;
  int size=0;
-while(temp>0)
+ while(temp>0)
+ {
+ t = (t*10) + temp%10;
+ temp = temp/10;
+ size+=1;
+ }
+ while(t>0)
 {
-t = (t*10) + temp%10;
-temp = temp/10;
-size+=1;
-}
-while(t>0)
-{
-st[i]=t%10+48;
-t/=10;
-i+=1;
-size-=1;
-}
-while(size--)
-{
-st[i]='0';
-i+=1;
-}
-st[i]='\0';
-l = strlen(st); // see how many characters are in text string.
-glRasterPos2i( x, y); // location to start printing text
-char ttt[10]={'S','c','o','r','e',':'};
-for(int i=0;i<6;i++)
-glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, ttt[i]); // Print a character on the screen
-for( i=0; i < l; i++) // loop until i is greater then l
-{
-glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, st[i]); // Print a character on the screen
-}
+ st[i]=t%10+48;
+ t/=10;
+ i+=1;
+ size-=1;
+ }
+ while(size--)
+ {
+  st[i]='0';
+  i+=1;
+ }
+ st[i]='\0';
+ l = strlen(st); // see how many characters are in text string.
+ glRasterPos2i( x, y); // location to start printing text
+ char ttt[10]={'S','c','o','r','e',':'};
+ for(int i=0;i<6;i++)
+  glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, ttt[i]); // Print a character on the screen
+ for( i=0; i < l; i++) // loop until i is greater then l
+ {
+   glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, st[i]); // Print a character on the screen
+ }
 }
 int main(int argc, char **argv) 
 {
     // Initialize GLUT
-   for (int i = 0; i < count; ++i)
+   for (int i = 0; i < 19; i++)
    {
-    for (int j = 0; j < count; ++j)
+    for (int j = 0; j < 19; j++)
      {
       flags[i][j]=0;
      }
@@ -243,26 +227,26 @@ void drawScene()
     glVertex2f((box_len / 2) + 0.26, -( box_len / 2) + 0.01 );
     glEnd();
     glPushMatrix();
-    Sprint(3,3);
+    Sprint(4.6f,3.0f);
    if(flag==0 || flag2==1 )
    {
-    glPushMatrix();
-    glBegin(GL_LINES);
-    glVertex2f(q.x,q.y);
-    glVertex2f(px,py-1.0f);
-    glEnd();
-    glPopMatrix();
+      glPushMatrix();
+      glBegin(GL_LINES);
+      glVertex2f(q.x,q.y);
+      glVertex2f(px,py-1.0f);
+      glEnd();
+      glPopMatrix();
     }
     glPushMatrix();
     glColor3f(1.0f,0.0f,0.0f);
     drawBall(0.15f);
     glPopMatrix();
    //Draw Board
+   //bottom side of carrom board.
     glPushMatrix();
     glColor3f(0.0f,0.0f,0.0f);
     drawcircle2(6*ball_rad);
     q.drawl(ball_x3+2.0f,ball_y3-0.5f+ball_rad-0.05f);
-    glColor3f(0.0f,0.0f,0.0f);
     q.drawl(ball_x3+2.0f,ball_y3-0.5f-ball_rad+0.05f);
     glPushMatrix();
     glTranslatef(ball_x3+2.0f,ball_y3-0.5f,0.0f);
@@ -534,14 +518,13 @@ void drawScene()
        glPopMatrix();
       }
      }  
-/*    // Draw Triangle
+    // Draw Triangle
     glPushMatrix();
     glTranslatef(tri_x, tri_y, 0.0f);
     glRotatef(theta, 0.0f, 0.0f, 1.0f);
-    glScalef(0.5f, 0.5f, 0.5f);
+    glScalef(0.05f, 0.05f, 0.05f);
     drawTriangle();
-    glPopMatrix();*/
-
+    glPopMatrix();
     glPopMatrix();
     glutSwapBuffers();
 }
@@ -718,11 +701,11 @@ void drawBall(float rad) {
 void drawTriangle() 
 {
     glBegin(GL_TRIANGLES);
-    glColor3f(0.0f, 0.0f, 1.0f);
+    glColor3f(0.0f, 0.0f, 0.0f);
     glVertex3f(0.0f, 1.0f, 0.0f);
-    glColor3f(0.0f, 1.0f, 0.0f);
+    glColor3f(0.0f, 0.0f, 0.0f);
     glVertex3f(-1.0f, -1.0f, 0.0f);
-    glColor3f(1.0f, 0.0f, 0.0f);
+    glColor3f(0.0f, 0.0f, 0.0f);
     glVertex3f(1.0f, -1.0f, 0.0f);
     glEnd();
 }
@@ -731,7 +714,7 @@ void initRendering() {
 
     glEnable(GL_DEPTH_TEST);        // Enable objects to be drawn ahead/behind one another
     glEnable(GL_COLOR_MATERIAL);    // Enable coloring
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);   // Setting a background color
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);   // Setting a background color
 }
 // Function called when the window is resized
 void handleResize(int w, int h) {
@@ -746,9 +729,7 @@ void handleResize(int w, int h) {
 
 void handleKeypress1(unsigned char key, int x, int y) {
 
-    float x1=0.1;
-     float r; 
-     char c=32;
+    float r; 
     if (key == 27) {
         exit(0);     // escape key is pressed
     }
@@ -796,7 +777,6 @@ void handleKeypress1(unsigned char key, int x, int y) {
 
      }
 }
-
 void handleKeypress2(int key, int x, int y) 
 {
 
@@ -815,36 +795,35 @@ void handleKeypress2(int key, int x, int y)
      }
      if(key == GLUT_KEY_UP)
       {
-        q.vx +=0.01;
-        q.vy +=0.01; 
-        r=((q.vx)*(q.vx)) + ((q.vy)*(q.vy));
-        r=sqrt(r);
-        q.vx =  (r * cos (o*PI/180)); 
-        q.vy =  (r * sin (o*PI/180));
-        poly_y1 += x1;
+        if(poly_y1 <= (box_len/2))
+        {  
+         q.vx +=0.01;
+         q.vy +=0.01; 
+         r=((q.vx)*(q.vx)) + ((q.vy)*(q.vy));
+         r=sqrt(r);
+         q.vx =  (r * cos (o*PI/180)); 
+         q.vy =  (r * sin (o*PI/180));
+         poly_y1 += x1;
+        }
       }
     if(key == GLUT_KEY_DOWN)
      {
-       q.vx -=0.01;
+        if(poly_y1 >=-(box_len/2))
+        {
+        q.vx -=0.01;
         q.vy -=0.01; 
         r=((q.vx)*(q.vx)) + ((q.vy)*(q.vy));
         r=sqrt(r);
         q.vx =  (r * cos (o*PI/180)); 
         q.vy =  (r * sin (o*PI/180));
         poly_y1 -=x1;
+        }
      }
     if(key == GLUT_KEY_PAGE_UP)
        {
        
        }    
-      // cout << key;
-      /* if(key == 116)
-       {
-         flag=1;
-         //q.vx=0.1f;
-         //q.vy=o.2f;
-        // update_striker();        
-       }*/
+     
 }
 void handleMouseclick(int button, int state, int x, int y)
 {
